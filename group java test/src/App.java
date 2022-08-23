579440d176dd9e729273a7b8d22d789a0b903361
tzvik15 <tzvik15@yahoo.com>
2022-08-23 08:43:03 -0400

    test app stage 1: simple push/pull, Daniel

--- group java test/src/App.java
+++
@@ -1,7 +0,0 @@
-//pull test Daniel
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
---
+++ group java test/src/GithubGroupTest.java
@@ -0,0 +1,11 @@
+
+/*
+Hi everyone! Please pull this repo, make a change to the output so it diplays your name as well, 
+and push it back. Once you did that, let me know. 
+*/
+
+public class GithubGroupTest {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Daniel pulled and pushed");
+    }
+}
