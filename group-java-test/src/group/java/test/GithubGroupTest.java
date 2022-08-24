b290341902987a335c36f7902692691ce7d8630b
jasonhowarth <jasonhowarth@macbookpro-j>
2022-08-24 07:49:54 -0400

    Changed name to Jason

---
+++ group-java-test/src/group/java/test/GithubGroupTest.java
@@ -0,0 +1,13 @@
+package group.java.test;
+
+
+/*
+Hi everyone! Please pull this repo, make a change to the output so it diplays your name as well, 
+and push it back. Once you did that, let me know. 
+*/
+
+public class GithubGroupTest {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Jason pulled and pushed");
+    }
+}
