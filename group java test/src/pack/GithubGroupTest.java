92e7bfeba1f986fc4060e6aaf62f6c380b4d8c82
JohnCMcGuire <monochromaticme@gmail.com>
2022-08-29 19:58:04 -0400

    This now prints my name (John).

--- group java test/src/pack/GithubGroupTest.java
+++ group java test/src/pack/GithubGroupTest.java
@@ -7,5 +7,6 @@ and push it back. Once you did that, let me know.
 public class GithubGroupTest {
     public static void main(String[] args) throws Exception {
         System.out.println("Daniel pulled and pushed");
+        System.out.println("John pulled and pushed.");
     }
 }
